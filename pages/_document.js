bc6506a9e9a2bdf927962174b01cd258266d1151
Susana Bustamante Tabares <167902901+Susanabt1001419@users.noreply.github.com>
2025-10-27 18:46:13 -0500

    Integrate Google Fonts in custom Document
    Add Google Fonts integration for Great Vibes font.

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,23 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        {/* Google Fonts: Great Vibes para los números del contador */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
 
