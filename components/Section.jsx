515bce0eb121246c6c92e47456b2ac22830582a9
Susana Bustamante Tabares <167902901+Susanabt1001419@users.noreply.github.com>
2025-10-27 17:39:25 -0500

    Add className and id props to Section component

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,8 +1,14 @@
+// components/Section.jsx
 import { motion } from 'framer-motion';
-export default function Section({ children }) {
+
+export default function Section({ children, className = "", id }) {
   return (
-    <section className='py-16 text-center'>
-      <motion.div initial={{opacity:0, y:20}} whileInView={{opacity:1, y:0}} transition={{duration:1}}>
+    <section id={id} className={`py-10 text-center ${className}`}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
         {children}
       </motion.div>
     </section>
