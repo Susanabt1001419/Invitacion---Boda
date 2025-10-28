68318429a2a9dc24843ec264e8099a865beca9ec
Susana Bustamante Tabares <167902901+Susanabt1001419@users.noreply.github.com>
2025-10-27 19:32:17 -0500

    Refactor tailwind.config.js fontFamily and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-fontFamily: {
-  greatvibes: ['"Great Vibes"', 'cursive'],
-},
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        serif: ['"Cormorant Garamond"', 'serif'],
-        script: ['"Great Vibes"', 'cursive'],
-        greatvibes: ['"Great Vibes"', 'cursive'], // para el contador
-      },
       colors: { beige: "#f8f5f0", gold: "#c6a664" },
+      fontFamily: {
+        serif: ['"Cormorant Garamond"', "serif"],
+        script: ['"Great Vibes"', "cursive"],
+        greatvibes: ['"Great Vibes"', "cursive"], // para el contador
+      },
     },
   },
   plugins: [],
-};
-  },
-  plugins: []
 };
