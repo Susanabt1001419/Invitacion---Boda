617c5d3113c62ff874a5a0cc636694f2bc34efda
Susana Bustamante Tabares <167902901+Susanabt1001419@users.noreply.github.com>
2025-12-13 17:29:58 -0500

    Fix video tag indentation in index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="min-h-screen bg-beige text-center font-serif">
 
       {/* 🌸 VIDEO HERO */}
-        <video
+      <video
         autoPlay
         loop
         muted
